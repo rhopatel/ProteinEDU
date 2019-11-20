87a524da089dd7a5197c93a5c4df2dc357bb48b5
rohanp12 <rohanp12@users.noreply.github.com>
2019-11-20 18:01:42 -0500

    Update FASTAtranslator.py

--- source/partOne/FASTAtranslator.py
+++ source/partOne/FASTAtranslator.py
@@ -1,8 +1,166 @@
 
-#this is where it begins
 from multi_key_dict import multi_key_dict
 import module_manager
+import copy
 
+#biological inheritance hierarchy:
+
+#AminoAcid superclass (will contain applicable method)
+class AminoAcid(object):
+    def __init__(self, sidechain):
+        self.alphaCarbon = AlphaCarbon()
+        self.amine = Amine()
+        self.carboxyl = Carboxyl()
+        self.sidechain = sidechain
+  
+
+#FunctionalGroup superclass (will contain applicable method)
+class FunctionalGroup(object):
+    def __init__(self, charge,sulfide,hbond):
+        self.charge = charge
+        self.sulfide = sulfide
+        self.hbond = hbond
+
+
+#three common functional groups
+class AlphaCarbon(FunctionalGroup):
+    def __init__(self):
+        super().__init__(0,False,False)
+
+class Amine(FunctionalGroup):
+    def __init__(self):
+        super().__init__(1,False,False)
+
+class Carboxyl(FunctionalGroup):
+    def __init__(self):
+        super().__init__(-1,False,False)
+
+
+#all the unique amino acids
+class Alanine(AminoAcid):
+    def __init__(self):
+        sidechain = Methyl()
+        super().__init__(sidechain)
+    
+
+class Arginine(AminoAcid): #TODO: add other sidechains
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Asparagine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+class AsparticAcid(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+class Cysteine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Glutamine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class GlutamicAcid(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Glycine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Histidine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Isoleucine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+class Leucine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Lysine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Methionine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+class Phenylalanine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Proline(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Serine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Threonine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Tryptophan(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Tyrosine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+class Valine(AminoAcid):
+    def __init__(self):    
+        sidechain = None 
+        super().__init__(sidechain)
+
+
+#unique sidechain functional groups (add the rest)
+class Methyl(FunctionalGroup):
+    def __init__(self):
+        super().__init__(0,False,False)
+
+
+
+#fasta file to AminoAcid sequence translator
 class FASTATranslator(object):
     def __init__(self, path):
         self.path = path
@@ -50,162 +208,40 @@ class FASTATranslator(object):
             if (codon == "stop" or len(codon)!=3):
                 break
             else:
-                aminoacidsequence.append(self.codonDict[codon])
+                aminoacidsequence.append(copy.copy(self.codonDict[codon]))
         return aminoacidsequence
 
 
     def makeCodonDict(self):
-        codonDict = multi_key_dict() #TODO: replace with amino acid objects
-        codonDict["UUU","UUC",] = "Phe"
-        codonDict["UUA","UUG","CUU","CUC","CUA","CUG"] = "Leu"
-        codonDict["AUU","AUC","AUA"] = "Ile"
-        codonDict["AUG"] = "Met"
-        codonDict["GUU","GUC","GUA","GUG"] = "Val"
-        codonDict["UCU","UCC","UCA","UCG","AGU","AGC"] = "Ser"
-        codonDict["CCU","CCC","CCA","CCG"] = "Pro"
-        codonDict["ACU","ACC","ACA","ACG"] = "Thr"
-        codonDict["GCU","GCC","GCA","GCG"] = "Ala"
-        codonDict["UAU","UAC"] = "Tyr"
+
+        codonDict = multi_key_dict() 
+        codonDict["UUU","UUC"] = Phenylalanine()
+        codonDict["UUA","UUG","CUU","CUC","CUA","CUG"] = Leucine()
+        codonDict["AUU","AUC","AUA"] = Isoleucine()
+        codonDict["AUG"] = Methionine()
+        codonDict["GUU","GUC","GUA","GUG"] = Valine()
+        codonDict["UCU","UCC","UCA","UCG","AGU","AGC"] = Serine()
+        codonDict["CCU","CCC","CCA","CCG"] = Proline()
+        codonDict["ACU","ACC","ACA","ACG"] = Threonine()
+        codonDict["GCU","GCC","GCA","GCG"] = Alanine()
+        codonDict["UAU","UAC"] = Tyrosine()
         codonDict["UAA","UAG","UGA"] = "stop"
-        codonDict["CAU","CAC"] = "His"
-        codonDict["CAA","CAG"] = "Gln"
-        codonDict["AAU","AAC"] = "Asn"
-        codonDict["AAA","AAG"] = "Lys"
-        codonDict["GAU","GAC"] = "Asp"
-        codonDict["GAA","GAG"] = "Glu"
-        codonDict["UGU","UGC"] = "Cys"
-        codonDict["UGG"] = "Trp"
-        codonDict["CGU","CGC","CGA","CGG","AGA","AGG"] = "Arg"
-        codonDict["GGU","GGC","GGA","GGG"] = "Gly"
+        codonDict["CAU","CAC"] = Histidine()
+        codonDict["CAA","CAG"] = Glutamine()
+        codonDict["AAU","AAC"] = Asparagine()
+        codonDict["AAA","AAG"] = Lysine()
+        codonDict["GAU","GAC"] = AsparticAcid()
+        codonDict["GAA","GAG"] = GlutamicAcid()
+        codonDict["UGU","UGC"] = Cysteine()
+        codonDict["UGG"] = Tryptophan()
+        codonDict["CGU","CGC","CGA","CGG","AGA","AGG"] = Arginine()
+        codonDict["GGU","GGC","GGA","GGG"] =  Glycine()
         return codonDict
 
     
 testTranslator = FASTATranslator("test.fasta")
 final = testTranslator.getSequence()
 module_manager.review()
-#print(final) 
+print(final) 
 
 
-class AminoAcid(object):
-    def __init__(self):
-        self.alphaCarbon = AlphaCarbon()
-        self.amine = Amine()
-        self.carboxyl = Carboxyl()
-
-class FunctionalGroup(object):
-    def __init__(self, charge,sulfide,hbond):
-        self.charge = charge
-        self.sulfide = sulfide
-        self.hbond = hbond
-
-class AlphaCarbon(FunctionalGroup):
-    def __init__(self):
-        super().__init__(self, 0,False,False)
-
-class Amine(FunctionalGroup):
-    def __init__(self):
-        super().__init__(self,1,False,False)
-
-class Carboxyl(FunctionalGroup):
-    def __init__(self):
-        super().__init__(self,-1,False,False)
-
-class Alanine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Arginine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Asparagine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class AsparticAcid(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Cysteine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Glutamine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class GlutamicAcid(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Glycine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Histidine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Isoleucine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Leucine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Lysine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Methionine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Phenylalanine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Proline(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Serine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Threonine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Tryptophan(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Tyrosine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
-
-class Valine(AminoAcid):
-    def __init__(self):
-        super.__init__(self)
-        self.sidechain = "idk"
